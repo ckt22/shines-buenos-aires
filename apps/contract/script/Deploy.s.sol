893cba4dc8900d094cfcf8f5269c15570d188a34
vincentho66 <vincent.ho66@yahoo.com.tw>
2025-11-23 02:56:44 -0300

    add alot alot and compile ok

--- apps/contract/script/Deploy.s.sol
+++ apps/contract/script/Deploy.s.sol
@@ -2,7 +2,7 @@
 pragma solidity ^0.8.20;
 
 import {Script, console} from "forge-std/Script.sol";
-import {TwitterEngagementRegistry} from "../src/TwitterEngagementRegistry.sol";
+import {TwitterEngagementRegistry} from "../src/TwitterVerifier.sol";
 
 contract DeployScript is Script {
     function run() external {
